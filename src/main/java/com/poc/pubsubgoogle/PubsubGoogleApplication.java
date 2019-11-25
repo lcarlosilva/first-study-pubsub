10b0a33fd39e294b3d46e9cea58caee95076314f
luizarruda <luizarruda@softbox.com.br>
2019-11-24 22:37:00 -0300

    initial structure

---
+++ src/main/java/com/poc/pubsubgoogle/PubsubGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.poc.pubsubgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubGoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/pubsubgoogle/PubsubGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.pubsubgoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubsubGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
